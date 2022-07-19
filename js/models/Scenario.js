8f3b3a0677149edbac7e46a34f3377e75575b161
matwork4 <51155034+matwork4@users.noreply.github.com>
2022-07-19 18:32:43 +0900

    Mei chan JS
    revoir les modèles sans question fermée

--- js/models/Scenario.js
+++ js/models/Scenario.js
@@ -2,14 +2,81 @@ class Scenario{
 
 	constructor(){
 		this.questions = [];
-		this.replies = [];
-		this.relationsQR = [];
+		this.replies = []; //Que des questions ouvertes ? + analyse syntaxique
+		this.relationsQR = []; //Pas besoin non plus
 		this.variables = [];
-		this.currentReplies = [];
+		this.currentReplies = []; //On le fait ou pas ? 
+		//Ou on remplace par de l'analyse syntaxique
 
 		this.isClosedQuestion = false;
 		this.isOpenQuestion = false;
-		this.isResumeScenario = false;
+		//this.isResumeScenario = false;
 	}
+
+	/* Ajoute une question a la fin de la liste
+	*/
+	addQuestion(q){
+		this.questions.push(q);
+	}
+
+	/* Retire une question en fonction de son ID
+	*/
+	removeQuestion(idQuestion){
+		for(let i=0; i<this.questions.length;i++){
+			if(this.questions[i].id == idQuestion){
+				//Retire 1 élément à l'indice i
+				this.questions.splice(i,1);
+			}
+		}
+	}
+
+	/* Ajoute une réponse a la fin de la liste
+	*/
+	addReply(r){
+		this.replies.push(r);
+	}
+
+	/* Retire une réponse en fonction de son ID
+	*/
+	removeReply(idReply){
+		for(let i=0; i<this.replies.length;i++){
+			if(this.replies[i].id == idReply){
+				this.replies.splice(i,1);
+			}
+		}
+	}
+
+	/* Ajoute une relation QR a la fin de la liste
+	*/
+	addRelationQR(r){
+		this.relationsQR.push(r);
+	}
+
+	/* Retire une relation QR en fonction de son ID
+	*/
+	removeRelationQR(idRelationQR){
+		for(let i=0; i<this.relationsQR.length;i++){
+			if(this.relationsQR[i].id == idRelationQR){
+				this.relationsQR.splice(i,1);
+			}
+		}
+	}
+
+	/* Ajoute une variable a la fin de la liste
+	*/
+	addVariable(v){
+		this.variables.push(v);
+	}
+
+	/* Retire une variable en fonction de son ID
+	*/
+	removeVariable(idVariable){
+		for(let i=0; i<this.variables.length;i++){
+			if(this.variables[i].id == idVariable){
+				this.variables.splice(i,1);
+			}
+		}
+	}
+
 
 }
--- js/views/mei_chan.js
+++ js/views/mei_chan.js
@@ -5,8 +5,50 @@
 function getSubmit(){
 	let element = document.getElementById("reply_field");
 	let text = element.value;
-	console.log(text);
+	//console.log(text);
+	addMessageHistory(text,true);
+	hideReply(true);
 }
+
+/* Ajoute un message dans le chat history
+ */
+function addMessageHistory(text, isSentByMe){
+
+	//On créer un nouvel élément p
+	let element = document.createElement("p");
+	element.innerHTML = text;
+	if(isSentByMe){
+		element.setAttribute("class","from-me");
+	}else{
+		element.setAttribute("class","from-them");
+	}
+
+	//On ajoute le message dans le DOM
+	document.getElementById("imessage").appendChild(element);
+}
+
+/* True : Cache la bulle de dialogue de Mei Chan
+*  False : re-Affiche
+*/
+function hideBubble(b){
+	if(b){
+		document.getElementById("bubble").style.visibility = "hidden";
+	}else{
+		document.getElementById("bubble").style.visibility = "visible";
+	}
+}
+
+/* True : Cache l'imput de l'utilisateur
+*  False : re-Affiche
+*/
+function hideReply(b){
+	if(b){
+		document.getElementById("reply").style.visibility = "hidden";
+	}else{
+		document.getElementById("reply").style.visibility = "visible";
+	}
+}
+
 
 
 
